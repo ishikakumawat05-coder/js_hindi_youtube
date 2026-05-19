581c7363b8a1d05b090413c4e75d70d2bf544f19
ishikakumawat05-coder <ishika.kumawat05@gmail.com>
2026-05-19 19:24:02 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId =1233
+let accountEmail ="ishi@google.com"
+var accountpassword =1234
+accountcity ="jaipur"
+let accountstate;
+
+//accountId = 1111(not allowed)
+accountEmail ="is@google.com"
+accountpassword =11111
+
+/*prefer not use to
+var becoz of issue in
+scope and functional scope*/
+console.log(accountId);
+console.log(accountEmail);
+
+console.table([accountEmail,accountId,accountpassword,accountcity,accountstate])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,6 @@
+"use strict"; //treat all JS code as new version
+
+//alert //we  are using nodejs
+let name="ishika"
+console.log("ishika")
+console.log(typeof"ishika");
